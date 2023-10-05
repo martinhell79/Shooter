012e2f56f877f0202e6445482bee9140be1b460e
Martin Hell <martin.hell@gmail.com>
2023-10-05 14:32:58 +0200

    add requirements.txt and .gitignore

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 #Set as desired
 
 # The size of non-flying objects
-OBJ_SIZE = 40  
+OBJ_SIZE = 20  
 
 # How much time to start with
 TIME_LIMIT = 30
 
 # Scale flying objects if images are not correct size
-scale_flying_object = 0.5
+scale_flying_object = 0.3
 
 # Set min and max speed of flying objects
 MIN_SPEED_FLYING_OBJECT = 100
