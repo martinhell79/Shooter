887eda8a8049a71532fdb88fcc1a6901bc74bb9a
Martin Hell <martin.hell@gmail.com>
2023-11-13 14:23:19 +0100

    Fixed typo

--- tools/extract_emails.py
+++ tools/extract_emails.py
@@ -9,7 +9,7 @@ def extract_unique_emails(file_names):
                 if len(parts) >= 3:  # Ensure there are at least 3 parts (points, name, email)
                     email = parts[2].strip()
                     unique_emails.add(email)
-
+    unique_emails.remove('Anonymous')
     return list(unique_emails)
 
 # Function to write unique emails to a new file with a specified separator
@@ -20,7 +20,7 @@ def write_unique_emails_to_file(unique_emails, output_file, separator):
 
 if __name__ == "__main__":
     # Specify the list of input file names and the output file name
-    input_files = ['hs1.txt', 'hs2.txt']  # Add your file names here
+    input_files = ['highscores-day1.txt', 'highscores-day2.txt', 'highscores-day3.txt']  # Add your file names here
     output_file = 'unique_emails.txt'  # Change to your desired output file name
 
     # Specify the separator used in the output file
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     # Write unique emails to the output file with the specified separator
     write_unique_emails_to_file(unique_emails, output_file, output_separator)
 
-    print(f"Unique emails written to '{output_file}' with separator '{output_separator}'")
+    print(f"Unique emails written to '{output_file}' with separator '{output_separator}'. (There are {len(unique_emails)} emails)")
